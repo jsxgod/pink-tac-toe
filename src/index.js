6c08bd8fec83740b409cf10ada08d86f66dca169
jsxgod <smykoo7@gmail.com>
2021-01-18 02:13:54 +0100

    Make Square component a functional one

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function Square(props) {
+const Square = (props) => {
     return (
         <button className="square" onClick={props.onClick}>
             {props.value}
